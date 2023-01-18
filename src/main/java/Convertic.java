49d7906d715e7be096c4eefea7c1e131c759a149
hervisConvertic <hervis.cifuentes@convertic.com.co>
2023-01-18 18:38:07 -0500

    se actualiza repositorio

--- src/main/java/Convertic.java
+++ src/main/java/Convertic.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Convertic {
 
     public static int sumIntegers(int x, int y) {
@@ -9,8 +13,18 @@ public class Convertic {
     }
 
     public static String stringUpperCase(String cadena) {
-
         return cadena.toUpperCase();
+    }
+    public static int listaNumeros(int a,int b, int c,int d,int e){
+        List<Integer> num=Arrays.asList(a,b,c,d,e);
+        int mayor=Integer.MIN_VALUE;
+
+        for(int numero:num){
+            if(numero>mayor){
+                mayor=numero;
+            }
+        }
+        return mayor;
     }
 
 }
--- src/test/java/TestConvertic.java
+++ src/test/java/TestConvertic.java
@@ -84,4 +84,52 @@ public class TestConvertic {
         Assertions.assertThrows(IllegalArgumentException.class,()->Convertic.stringUpperCase(texto));
     }
 
+    //ejercicio que entrega el mayo de
+    @Test
+    @DisplayName("crear la instancia de un objeto")
+    public void instanciaDeObj() {
+        int a=8;
+        int b=16;
+
+
+
+        //act
+      /*  int result = Convertic.listaNumeros(a,b,c,d,e);
+
+        //Assert &&assert
+        Assertions.assertEquals(16, result);*/
+    }
+
+    @Test
+    @DisplayName("mayor de una lista de numeros")
+    public void mayorNumero() {
+        int a=8;
+        int b=16;
+        int c=2;
+        int d=9;
+        int e=7;
+
+
+        //act
+        int result = Convertic.listaNumeros(a,b,c,d,e);
+
+        //Assert &&assert
+        Assertions.assertEquals(16, result);
+    }
+    @Test
+    @DisplayName("test de numero mayor")
+    public void listInteerstFailled() {
+        int a=8;
+        int b=16;
+        int c=2;
+        int d=9;
+        Integer e=2;
+
+        //act
+        //int result=Convertic.sumIntegers(s,d);
+
+        //assert&&assert
+        Assertions.assertThrows(NullPointerException.class, () -> Convertic.listaNumeros(a,b,c,d,e));
+    }
+
 }
